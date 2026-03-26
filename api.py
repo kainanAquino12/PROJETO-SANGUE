d43e6765082b7f31e6ea4d422dacbd15e554c20d
Kainan Aquino <kainanneres262@gmail.com>
2026-03-26 15:41:27 -0300

    adiciona rota POST estoque

--- api.py
+++ api.py
@@ -36,6 +36,47 @@ def criar_doadores():
 app.json.sort_keys = False
 @app.route('/doadores', methods=['GET'])
 
+@app.post('/estoque')
+def cria_estoque():
+    dados = request.get_json()
+
+    if not dados.get('estoque'):
+        return jsonify({"error": "O campo 'estoque' é obrigatório."}), 400
+        
+
+    with open ('estoque.json', 'r') as f:
+        estoque = json.load(f)
+    estoque.append(dados)
+
+    with open ('estoque.json', 'w') as f:
+        json.dump(estoque, f, indent=4, ensure_ascii=False)
+
+    resposta = {
+        "message": "Estoque criado com sucesso",  
+    }
+    return jsonify(resposta), 201
+
+@app.post('/sangue')
+def criar_sangue():
+    dados = request.get_json()
+
+    if not dados.get('sangue'):
+        return jsonify({"error": "O campo 'sangue' é obrigatório."}), 400
+        
+
+    with open ('sangue.json', 'r') as f:
+        sangue = json.load(f)
+    sangue.append(dados)
+
+    with open ('sangue.json', 'w') as f:
+        json.dump(sangue, f, indent=4, ensure_ascii=False)
+
+    resposta = {
+        "message": "Sangue criado com sucesso",  
+    }
+    return jsonify(resposta), 201
+
+
 def listar_doadores():
     
     with open('doadores.json', 'r') as f:
